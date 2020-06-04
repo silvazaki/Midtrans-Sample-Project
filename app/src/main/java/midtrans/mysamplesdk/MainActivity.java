ef6e1970efffedc7aa53d93ef349650fbcc72605
Silva Ahmad <silvaahmad.sa@gmail.com>
2020-06-04 19:55:50 +0700

    Initial commit

---
+++ app/src/main/java/midtrans/mysamplesdk/MainActivity.java
@@ -0,0 +1,220 @@
+package midtrans.mysamplesdk;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.midtrans.sdk.corekit.callback.CardRegistrationCallback;
+import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback;
+import com.midtrans.sdk.corekit.core.MidtransSDK;
+import com.midtrans.sdk.corekit.core.PaymentMethod;
+import com.midtrans.sdk.corekit.core.TransactionRequest;
+import com.midtrans.sdk.corekit.core.themes.CustomColorTheme;
+import com.midtrans.sdk.corekit.models.BankType;
+import com.midtrans.sdk.corekit.models.CardRegistrationResponse;
+import com.midtrans.sdk.corekit.models.CustomerDetails;
+import com.midtrans.sdk.corekit.models.ItemDetails;
+import com.midtrans.sdk.corekit.models.snap.CreditCard;
+import com.midtrans.sdk.corekit.models.snap.TransactionResult;
+import com.midtrans.sdk.uikit.SdkUIFlowBuilder;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements TransactionFinishedCallback {
+    private Button buttonUiKit, buttonDirectCreditCard, buttonDirectBcaVa, buttonDirectMandiriVa,
+            buttonDirectBniVa, buttonDirectAtmBersamaVa, buttonDirectPermataVa;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+        // SDK initiation for UIflow
+        initMidtransSdk();
+        initActionButtons();
+    }
+
+
+    private TransactionRequest initTransactionRequest() {
+        // Create new Transaction Request
+        TransactionRequest transactionRequestNew = new
+                TransactionRequest(System.currentTimeMillis() + "", 20000);
+
+        //set customer details
+        transactionRequestNew.setCustomerDetails(initCustomerDetails());
+
+
+        // set item details
+        ItemDetails itemDetails = new ItemDetails("1", 20000, 1, "Trekking Shoes");
+
+        // Add item details into item detail list.
+        ArrayList<ItemDetails> itemDetailsArrayList = new ArrayList<>();
+        itemDetailsArrayList.add(itemDetails);
+        transactionRequestNew.setItemDetails(itemDetailsArrayList);
+
+
+        // Create creditcard options for payment
+        CreditCard creditCard = new CreditCard();
+
+        creditCard.setSaveCard(false); // when using one/two click set to true and if normal set to  false
+
+//        this methode deprecated use setAuthentication instead
+//        creditCard.setSecure(true); // when using one click must be true, for normal and two click (optional)
+
+        creditCard.setAuthentication(CreditCard.AUTHENTICATION_TYPE_3DS);
+
+        // noted !! : channel migs is needed if bank type is BCA, BRI or MyBank
+//        creditCard.setChannel(CreditCard.MIGS); //set channel migs
+        creditCard.setBank(BankType.BCA); //set spesific acquiring bank
+
+        transactionRequestNew.setCreditCard(creditCard);
+
+        return transactionRequestNew;
+    }
+
+    private CustomerDetails initCustomerDetails() {
+
+        //define customer detail (mandatory for coreflow)
+        CustomerDetails mCustomerDetails = new CustomerDetails();
+        mCustomerDetails.setPhone("085310102020");
+        mCustomerDetails.setFirstName("user fullname");
+        mCustomerDetails.setEmail("mail@mail.com");
+        return mCustomerDetails;
+    }
+
+    private void initMidtransSdk() {
+        String client_key = SdkConfig.MERCHANT_CLIENT_KEY;
+        String base_url = SdkConfig.MERCHANT_BASE_CHECKOUT_URL;
+
+        SdkUIFlowBuilder.init()
+                .setClientKey(client_key) // client_key is mandatory
+                .setContext(this) // context is mandatory
+                .setTransactionFinishedCallback(this) // set transaction finish callback (sdk callback)
+                .setMerchantBaseUrl(base_url) //set merchant url
+                .enableLog(true) // enable sdk log
+                .setColorTheme(new CustomColorTheme("#FFE51255", "#B61548", "#FFE51255")) // will replace theme on snap theme on MAP
+                .buildSDK();
+    }
+
+    @Override
+    public void onTransactionFinished(TransactionResult result) {
+        if (result.getResponse() != null) {
+            switch (result.getStatus()) {
+                case TransactionResult.STATUS_SUCCESS:
+                    Toast.makeText(this, "Transaction Finished. ID: " + result.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                    break;
+                case TransactionResult.STATUS_PENDING:
+                    Toast.makeText(this, "Transaction Pending. ID: " + result.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                    break;
+                case TransactionResult.STATUS_FAILED:
+                    Toast.makeText(this, "Transaction Failed. ID: " + result.getResponse().getTransactionId() + ". Message: " + result.getResponse().getStatusMessage(), Toast.LENGTH_LONG).show();
+                    break;
+            }
+            result.getResponse().getValidationMessages();
+        } else if (result.isTransactionCanceled()) {
+            Toast.makeText(this, "Transaction Canceled", Toast.LENGTH_LONG).show();
+        } else {
+            if (result.getStatus().equalsIgnoreCase(TransactionResult.STATUS_INVALID)) {
+                Toast.makeText(this, "Transaction Invalid", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Transaction Finished with failure.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void bindViews() {
+        buttonUiKit = findViewById(R.id.button_uikit);
+        buttonDirectCreditCard = findViewById(R.id.button_direct_credit_card);
+        buttonDirectBcaVa = findViewById(R.id.button_direct_bca_va);
+        buttonDirectMandiriVa = findViewById(R.id.button_direct_mandiri_va);
+        buttonDirectBniVa = findViewById(R.id.button_direct_bni_va);
+        buttonDirectPermataVa = findViewById(R.id.button_direct_permata_va);
+        buttonDirectAtmBersamaVa = findViewById(R.id.button_direct_atm_bersama_va);
+
+    }
+
+    private void initActionButtons() {
+        buttonUiKit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this);
+            }
+        });
+
+        buttonDirectCreditCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().UiCardRegistration(MainActivity.this, new CardRegistrationCallback() {
+                    @Override
+                    public void onSuccess(CardRegistrationResponse cardRegistrationResponse) {
+                        Toast.makeText(MainActivity.this, "register card token success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(CardRegistrationResponse cardRegistrationResponse, String s) {
+                        Toast.makeText(MainActivity.this, "register card token Failed", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+
+                    }
+                });
+            }
+        });
+
+
+        buttonDirectBcaVa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this, PaymentMethod.BANK_TRANSFER_BCA);
+            }
+        });
+
+
+        buttonDirectBniVa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this, PaymentMethod.BANK_TRANSFER_BNI);
+            }
+        });
+
+        buttonDirectMandiriVa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this, PaymentMethod.BANK_TRANSFER_MANDIRI);
+            }
+        });
+
+
+        buttonDirectPermataVa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this, PaymentMethod.BANK_TRANSFER_PERMATA);
+            }
+        });
+
+        buttonDirectAtmBersamaVa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MidtransSDK.getInstance().setTransactionRequest(initTransactionRequest());
+                MidtransSDK.getInstance().startPaymentUiFlow(MainActivity.this, PaymentMethod.BCA_KLIKPAY);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/midtrans/mysamplesdk/SdkConfig.java
@@ -0,0 +1,12 @@
+package midtrans.mysamplesdk;
+
+/**
+ * Created by ziahaqi on 8/30/17.
+ */
+
+public class SdkConfig {
+//    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://app.sandbox.midtrans.com/snap/v2/vtweb/UsU943qTp0oE9AAF/";
+//    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://api.sandbox.veritrans.co.id/v2/UsU943qTp0oE9AAF/";
+    public static final String MERCHANT_BASE_CHECKOUT_URL = "https://api.bobobox.co.id/midtrans/v1/";
+    public static final String MERCHANT_CLIENT_KEY = "Mid-client-hXrFJZ2x6Z2QPfIl";
+}
